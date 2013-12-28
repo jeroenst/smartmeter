1042ad9f3f33855fabbc9425a39ca6641390d05d
jeroensteenhuis <github@jeroensteenhuis.nl>
2013-12-28 15:11:59 +0100

    Writing /tmp/smartmeter.xml for webconsole

--- smartmeter.php
+++ smartmeter.php
@@ -1,4 +1,4 @@
-#!php
+#!/usr/bin/php
 <?php  
  include "php_serial.class.php";  
 
@@ -26,9 +26,11 @@ $serial->confFlowControl("none");
 $serialready=0;
 $receivedpacket="";
 $start_time = time();
+$write_database_timeout = 10; // write database every 10 minutes
+$write_database_timer = time();
 
 $Electricity_Usage = 0;
-$Eelectricity_Used_1 = 0;
+$Electricity_Used_1 = 0;
 $Electricity_Used_2 = 0;
 $Electricity_Provided_1 = 0;
 $Electricity_Provided_2 = 0;
@@ -62,6 +64,7 @@ echo "Opening Serial Port...\n";
                 if($label[1] == "1-0:1.7.0") $Electricity_Usage = extractfloat($value[1]) * 1000;
                 if($label[1] == "1-0:1.8.1") $Electricity_Used_1 = extractfloat($value[1]);
                 if($label[1] == "1-0:1.8.2") $Electricity_Used_2 = extractfloat($value[1]);
+                $Electricity_Used = $Electricity_Used_1 + $Electricity_Used_2;
                 if($label[1] == "1-0:2.8.1") $Electricity_Provided_1 = extractfloat($value[1]);
                 if($label[1] == "1-0:2.8.2") $Electricity_Provided_2 = extractfloat($value[1]);
                 if($label[1] == "") $Gas_Used = extractfloat($value[1]);
@@ -70,7 +73,65 @@ echo "Opening Serial Port...\n";
               {
                 $receivedpacket = "";
                 echo "Received Data (".date('Y/m/d H:i:s')."): gas_used=$Gas_Used, kwh_used1=$Electricity_Used_1, kwh_used2=$Electricity_Used_2, kwh_provided1=$Electricity_Provided_1, kwh_provided2= $Electricity_Provided_2, watt_usage=$Electricity_Usage\n";
-                writeEnergyDatabase($Gas_Used, $Electricity_Used_1, $Electricity_Used_2, $Electricity_Provided_1, $Electricity_Provided_2, $Electricity_Usage);
+                if ($write_database_timer < time() )
+                {
+                  $write_database_timer = time() + ($write_database_timeout * 60);
+                  echo "Writing values to database...";
+                  writeEnergyDatabase($Gas_Used, $Electricity_Used_1, $Electricity_Used_2, $Electricity_Provided_1, $Electricity_Provided_2, $Electricity_Usage);
+
+		  // Get all the other values from the domotica database  if Gas_Used or Electricity_Used has changed, but only when XMBC is not playing...
+		  $mysqlresult = mysql_query("SELECT * FROM `energy` WHERE `timestamp` >= timestampadd(hour, -1, now()) LIMIT 1");
+		  $Electricity_Used_Hour=$Electricity_Used - (mysql_result($mysqlresult, 0, "kwh_used1") + mysql_result($mysqlresult, 0, "kwh_used2"));
+		  $Gas_Used_Hour=$Gas_Used - mysql_result($mysqlresult, 0, "gas_used");
+		  $Gas_Usage=round($Gas_Used_Hour * 1000);
+
+
+		  $mysqlresult = mysql_query("SELECT * FROM `energy` WHERE `timestamp` >= CURDATE() LIMIT 1");
+    $Electricity_Used_Today=$Electricity_Used - (mysql_result($mysqlresult, 0, "kwh_used1") + mysql_result($mysqlresult, 0, "kwh_used2"));
+    $Gas_Used_Today=$Gas_Used - mysql_result($mysqlresult, 0, "gas_used");
+
+    $mysqlresult = mysql_query("SELECT * FROM `energy` WHERE `timestamp` >= DATE_SUB(CURDATE(),INTERVAL 7 DAY) LIMIT 1");
+    $Electricity_Used_Week=$Electricity_Used - (mysql_result($mysqlresult, 0, "kwh_used1") + mysql_result($mysqlresult, 0, "kwh_used2"));
+    $Gas_Used_Week=$Gas_Used - mysql_result($mysqlresult, 0, "gas_used");
+
+    $mysqlresult = mysql_query("SELECT * FROM `energy` WHERE `timestamp` >= DATE_SUB(CURDATE(),INTERVAL 30 DAY) LIMIT 1");
+    $Electricity_Used_Month=$Electricity_Used - (mysql_result($mysqlresult, 0, "kwh_used1") + mysql_result($mysqlresult, 0, "kwh_used2"));
+    $Gas_Used_Month=$Gas_Used - mysql_result($mysqlresult, 0, "gas_used");
+
+    $mysqlresult = mysql_query("SELECT * FROM `energy` WHERE `timestamp` >= DATE_SUB(CURDATE(),INTERVAL 365 DAY) LIMIT 1");
+    $Electricity_Used_Year=$Electricity_Used - (mysql_result($mysqlresult, 0, "kwh_used1") + mysql_result($mysqlresult, 0, "kwh_used2"));
+    $Gas_Used_Year=$Gas_Used - mysql_result($mysqlresult, 0, "gas_used");
+
+                }
+
+
+                echo "Writing values to xml file...\n";
+    
+                $xml = new SimpleXMLElement('<root/>');
+                $xml->addChild("Electricity_Usage" , $Electricity_Usage);
+                $xml->addChild("Electricity_Used" , $Electricity_Used);
+                $xml->addChild("Electricity_Used_1" , $Electricity_Used_1);
+                $xml->addChild("Electricity_Used_2" , $Electricity_Used_2);
+                $xml->addChild("Electricity_Used_Today" , $Electricity_Used_Today);
+                $xml->addChild("Electricity_Used_Hour" , $Electricity_Used_Hour);
+                $xml->addChild("Electricity_Used_Week" , $Electricity_Used_Week);
+                $xml->addChild("Electricity_Used_Month" , $Electricity_Used_Month);
+                $xml->addChild("Electricity_Used_Year" , $Electricity_Used_Year);
+                $xml->addChild("Electricity_Provided" , $Electricity_Provided_1+$Electricity_Provided_2);
+                $xml->addChild("Electricity_Provided1" , $Electricity_Provided_1);
+                $xml->addChild("Electricity_Provided2" , $Electricity_Provided_2);
+                $xml->addChild("Gas_Usage" , $Gas_Usage);
+                $xml->addChild("Gas_Used" , $Gas_Used);
+                $xml->addChild("Gas_Used_Today" , $Gas_Used_Today);
+                $xml->addChild("Gas_Used_Hour" , $Gas_Used_Hour);
+                $xml->addChild("Gas_Used_Week" , $Gas_Used_Week);
+                $xml->addChild("Gas_Used_Month" , $Gas_Used_Month);
+                $xml->addChild("Gas_Used_Year" , $Gas_Used_Year);
+            
+                $dom = dom_import_simplexml($xml)->ownerDocument;
+                $dom->formatOutput = true;
+                file_put_contents('/tmp/smartmeter.xml.tmp', $dom->saveXML(), LOCK_EX);
+                exec ('mv /tmp/smartmeter.xml.tmp /tmp/smartmeter.xml');
               }
             } 
           }
@@ -149,7 +210,7 @@ function writeEnergyDatabase($gas_used, $kwh_used1, $kwh_used2, $kwh_provided1, 
   static $Mysql_con;
   if (!$Mysql_con)
   {
-      $Mysql_con = mysql_connect("nas","domotica","b-2020");
+      $Mysql_con = mysql_connect("server","domotica","dom8899");
   }
 
   if ($Mysql_con)
